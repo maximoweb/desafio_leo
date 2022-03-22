319d988ef644528fc84cd7ed0333f2ee9889c412
maximoweb <maximoweb@users.noreply.github.com>
2022-03-22 12:23:32 -0300

    Criada a Acão CREATE curso

--- App/Ajax/gerenciar-cursos.php
+++ App/Ajax/gerenciar-cursos.php
@@ -29,14 +29,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         //Geralmente utilizao essa comparação caso o usuario esteja com a aba do form aberta com um usuário e abre outra com outro usuário/sessao
         if (empty($USU_ID) || session_id() != $form["CURSO_SESSION_ID"]) throw new Exception("Inconsistencia na Session");
 
-        $ID = $form[$CAMPOID]; //atribuindo à variavel ID = Id do campo
-        unset($form[$CAMPOID]); //Removendo o campo ID para não entrar como campo na Tabela
+        $ID = $form["CURSO_ID"]; //atribuindo à variavel ID = Id do campo
+        unset($form["CURSO_ID"]); //Removendo o campo ID para não entrar como campo na Tabela
         $form["CURSO_USU_ID"] = $USU_ID;
         if (is_numeric($ID)) {
             if ($ID == 0) //inserir
             {
+                $create = Crud::Create("desleo_cursos", $form);
+                if ($create["sts"] == "erro") throw new Exception($create["msg"]);
+                $ret["msg"] = "Curso Inserido com sucesso!";
+                $ret["id"] = $create["id"];
             } else { //update
-
+                foreach ($form as $ch => $val) {
+                    $camposSet[] = "`$ch` = '$val'";
+                }
+                $camposSet = implode(", ", $camposSet);
+                $atualizar = "update `desleo_cursos` set $camposSet where `$CAMPOID` = $ID ";
+                //$ret["query"] = $atualizar;
+                if (!mysqli_query($conn, $atualizar)) throw new Exception(mysqli_error($conn));
+                $ret["msg"] = "Curso Atualizado com sucesso!";
+                $ret["id"] = $ID;
             }
         }
         $ret["sts"] = "ok";
