aebd21a7c2fcd028953264d5255c78ffc3e5f87d
maximoweb <maximoweb@users.noreply.github.com>
2022-03-22 12:13:30 -0300

    Criando Crud Create

--- App/Class/Crud.php
+++ App/Class/Crud.php
@@ -6,6 +6,31 @@ require __DIR__ . '/InOut.php';
 
 class Crud extends Conexao
 {
+
+    static public function Create($table, $campos = array())
+    {
+        $con = parent::Conn();
+        #FILTER_SANITIZE_ADD_SLASHES - escapa caracteres especiais (\',\\)
+        $campos = filter_var_array($campos, FILTER_SANITIZE_ADD_SLASHES);
+        $campos = InOut::setDataIn($campos);
+        $cps = "`" . implode("`,`", array_keys($campos)) . "`";
+        $values = "'" . implode("','", $campos) . "'";
+        $insert =  "insert into `$table` ($cps) values ($values) ";
+        $ret["crud"] = 'create';
+        try {
+            if (!mysqli_query($con, $insert)) {
+                throw new Exception(mysqli_error($con));
+            }
+            $ret['sts'] = 'ok';
+            $ret["id"] = mysqli_insert_id($con);
+        } catch (Exception $e) {
+            $ret['sts'] = 'erro';
+            $ret["erro"] = $e->getMessage();
+            $ret["msg"] = $ret["erro"];
+        }
+        return $ret;
+    }
+
     static public function Read($table, $where, $campos = [])
     {
         $con = parent::Conn();
