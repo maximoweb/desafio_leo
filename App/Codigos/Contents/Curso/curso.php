40e6bdb29e40996cb287180c18374a4c503bdabe
maximoweb <maximoweb@users.noreply.github.com>
2022-03-23 20:31:29 -0300

    Paginas Saiba Mais

--- App/Codigos/Contents/Curso/curso.php
+++ App/Codigos/Contents/Curso/curso.php
@@ -1,7 +1,9 @@
 <?php
 //caminhorDir - Pegando caminho do Config/Router
 // Mas pode ser colocado Manual (igual Acima)
-$CPS = [
-    'banner' => 'xxx'
-];
-echo View::Include("Contents/" . caminhorDir, $CPS);
+
+
+$CURSO_URL_AMIGAVEL =  $router[1];
+
+$CURSO = Crud::Read("desleo_cursos", "where CURSO_URL_AMIGAVEL = '$CURSO_URL_AMIGAVEL'", ['*']);
+echo View::Include("Contents/" . caminhorDir, $CURSO[0]);
